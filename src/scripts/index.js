ca73cc61b890b5a0e89d4b4dc87a0714606fd671
Andrey Yamanov <tenphi@gmail.com>
2020-03-16 14:34:26 +0300

    add dark scheme and high contrast switches

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import '../../numl/numl.dev';
 let logoUrl = require('../images/logo.svg');
 
 setTimeout(() => {
+  const html = document.querySelector('html');
+
   Nude.getElementById('logo').setAttribute('src', logoUrl);
 
   const hueSlider = Nude.getElementById('hue-slider');
@@ -16,16 +18,79 @@ setTimeout(() => {
     hueEl.innerHTML = hue;
   });
 
-  const snippet = Nude.getElementById('snippet');
+  // TOGGLE SCHEME
+  const toggle = Nude.getElementById('scheme-switch');
 
-  snippet.querySelector('textarea').textContent = `<nu-btn>
-  Dropdown
-  <nu-icon
-    name="chevron-down" size="1.5"
-    transition="scale" color="special"
-    scale="^:pressed[flip-y]"></nu-icon>
-  <nu-popup>Content</nu-popup>
-</nu-btn>`;
+  function toggleScheme() {
+    let scheme;
+
+    if (html.classList.contains('nu-scheme-dark')) {
+      scheme = 'light';
+    } else if (html.classList.contains('nu-scheme-light')) {
+      scheme = 'dark';
+    } else {
+      scheme = matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
+    }
+
+    if (scheme === 'light') {
+      html.classList.remove('nu-scheme-dark');
+      html.classList.add('nu-scheme-light');
+    } else {
+      html.classList.remove('nu-scheme-light');
+      html.classList.add('nu-scheme-dark');
+    }
+
+    setToggleStyle(scheme);
+  }
+
+  function setToggleStyle(scheme) {
+    if (scheme === 'dark') {
+      toggle.setAttribute('checked', '');
+    } else {
+      toggle.removeAttribute('checked');
+    }
+  }
+
+  setToggleStyle(matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+  toggle.addEventListener('tap', toggleScheme);
+
+  // TOGGLE CONTRAST
+  const toggleContrastEl = Nude.getElementById('contrast-switch');
+
+  function toggleContrast() {
+    let contrast;
+
+    if (html.classList.contains('nu-contrast-high')) {
+      contrast = 'low';
+    } else if (html.classList.contains('nu-contrast-low')) {
+      contrast = 'high';
+    } else {
+      contrast = matchMedia('(prefers-contrast: high)').matches ? 'low' : 'high';
+    }
+
+    if (contrast === 'low') {
+      html.classList.remove('nu-contrast-high');
+      html.classList.add('nu-contrast-low');
+    } else {
+      html.classList.remove('nu-contrast-low');
+      html.classList.add('nu-contrast-high');
+    }
+
+    setToggleContrastStyle(contrast);
+  }
+
+  function setToggleContrastStyle(contrast) {
+    if (contrast === 'high') {
+      toggleContrastEl.setAttribute('checked', '');
+    } else {
+      toggleContrastEl.removeAttribute('checked');
+    }
+  }
+
+  setToggleContrastStyle(matchMedia('(prefers-contrast: high)').matches ? 'high' : 'low');
+
+  toggleContrastEl.addEventListener('tap', toggleContrast);
 });
 
 // FIX FAVICON
